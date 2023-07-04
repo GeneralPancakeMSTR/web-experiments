f6b629153dcb7ba0e85703059f05f4831e3e46f5
generalpancakemstr <generalpancake@gmail.com>
2023-07-04 10:28:00 -0600

    SocketIO implementation.

--- src/server/server.ts
+++ src/server/server.ts
@@ -5,6 +5,12 @@ const app = express();
 const hostname = '0.0.0.0';
 const port = 8000;
 
+import * as http from 'http'; 
+const server = http.createServer(app);
+
+import {Server} from 'socket.io';
+const io = new Server(server);
+
 app.use(express.static(path.join(__dirname,'../dist')));
 
 app.get('/',(req:express.Request,res:express.Response)=>{
@@ -12,6 +18,11 @@ app.get('/',(req:express.Request,res:express.Response)=>{
     res.sendFile(path.join(__dirname,'../dist/index.html'));
 });
 
-app.listen(port,hostname, ()=> {
+io.on('connection',(socket) => {
+    console.log('A user connected');
+    io.emit('test-log',socket.id);
+});
+
+server.listen(port,hostname, () => {
     console.log(`[server]: Server is running at http://${hostname}:${port}`);
 });
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,3 +1,8 @@
 import {babylon_app} from './babylon';
+import {test_client} from './socket_clients';
+
+const socketio_log = new test_client(); 
 
 new babylon_app();
+
+socketio_log.appendLog(); 
